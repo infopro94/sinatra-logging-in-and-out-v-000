8a5d4c0234692a29703ef1f67847cf19728678e8
Susan Fowler <infopro94@gmail.com>
2018-01-11 23:05:43 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 require_relative '../../config/environment'
 class ApplicationController < Sinatra::Base
   configure do
+    set :public_folder, 'public'
     set :views, Proc.new { File.join(root, "../views/") }
     enable :sessions unless test?
     set :session_secret, "secret"
@@ -20,12 +21,16 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/account' do
-
+    @current_user = User.find_by_id(session[:user_id])
+    if @current_user 
+      erb :account
+    else 
+      erb :error
   end
 
   get '/logout' do
-
+    session.clear
+    redirect to '/'
   end
-
 
 end
